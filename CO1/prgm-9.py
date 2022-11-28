a5aaae2462b7bc589d7a94457fbf3f92fcf0d88e
Akash Mohankumar M <90025516+AkashMohan0301@users.noreply.github.com>
2022-11-28 21:26:22 +0530

    Update prgm-9.py

--- CO1/prgm-9.py
+++ CO1/prgm-9.py
@@ -1,5 +1,5 @@
 #9-Create a string from input string where first and last word exchanged ??
-word = input("Enter the string")
-word_new = word[-1]+word[1:-2]+word[0]
-print(word)
-print(word_new)
+str = input("input a string")
+print(str)
+str = str[-1]+str[1:-1]+str[0]
+print(str)
